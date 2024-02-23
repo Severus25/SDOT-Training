d78bc4df37b9e21c41b9fa71678a72f8be859889
Abhijai Rajawat <77570887+Severus25@users.noreply.github.com>
2024-02-23 21:30:34 +0530

    Create 1. ImplementStackusingQueues.py

---
+++ Day - 4 (Stacks and Queues)/1. ImplementStackusingQueues.py
@@ -0,0 +1,57 @@
+# Leetcode - 225
+# Using 1 Queue
+class MyStack:
+
+    def __init__(self):
+        self.q = deque()
+
+    def push(self, x: int) -> None:
+        self.q.append(x)
+        for _ in range(len(self.q) - 1):
+            self.q.append(self.q.popleft())
+
+    def pop(self) -> int:
+        return self.q.popleft()
+
+    def top(self) -> int:
+        return self.q[0]
+
+    def empty(self) -> bool:
+        return len(self.q) == 0
+
+  #Using 2 Queues
+  class MyStack:
+
+    def __init__(self):
+        self.q1 = deque()
+        self.q2 = deque()
+
+    def push(self, x: int) -> None:
+        self.q1.append(x)
+
+    def pop(self) -> int:
+        while len(self.q1) > 1:
+            self.q2.append(self.q1.popleft())
+        
+        popped_element = self.q1.popleft()
+        
+        # Swap q1 and q2
+        self.q1, self.q2 = self.q2, self.q1
+        
+        return popped_element
+
+    def top(self) -> int:
+        while len(self.q1) > 1:
+            self.q2.append(self.q1.popleft())
+        
+        top_element = self.q1[0]
+        
+        self.q2.append(self.q1.popleft())
+        
+        # Swap q1 and q2
+        self.q1, self.q2 = self.q2, self.q1
+        
+        return top_element
+
+    def empty(self) -> bool:
+        return len(self.q1) == 0
