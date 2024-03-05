a4c74738f9807fd21ec81ef27ca148c620a3df7c
Abhijai Rajawat <77570887+Severus25@users.noreply.github.com>
2024-03-06 00:01:50 +0530

    Create 3. FindinMountainArray.py

---
+++ Day - 5 (Dynamic Programming)/3. FindinMountainArray.py
@@ -0,0 +1,48 @@
+#Leetcode - 1095
+class Solution:
+    def findInMountainArray(self, target: int, mountain_arr: 'MountainArray') -> int:
+        length = mountain_arr.length()
+
+        def find_target(left, right, target, is_upside):
+            while left <= right:
+                mid = (left + right) // 2
+                mid_val = mountain_arr.get(mid)
+
+                if mid_val == target:
+                    return mid
+                
+                if is_upside:
+                    if target > mid_val:
+                        left = mid + 1
+                    else:
+                        right = mid - 1
+                else:
+                    if target > mid_val:
+                        right = mid - 1
+                    else:
+                        left = mid + 1
+
+            return -1
+
+        def find_peak():
+            nonlocal length
+
+            left, right = 0, length - 1
+
+            while left < right:
+                mid = (left + right) // 2
+                if mountain_arr.get(mid) < mountain_arr.get(mid + 1):
+                    left = mid + 1
+                else:
+                    right = mid
+            
+            return left
+
+        peak_index = find_peak()
+
+
+        result = find_target(0, peak_index, target, True)
+        if result != -1:
+            return result
+        
+        return find_target(peak_index + 1, length - 1, target, False)  
