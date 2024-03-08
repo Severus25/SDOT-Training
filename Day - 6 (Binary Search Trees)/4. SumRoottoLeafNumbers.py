abcfd7a0519abc836a36f3d4113ab06fa25fea6b
Abhijai Rajawat <77570887+Severus25@users.noreply.github.com>
2024-03-08 12:01:26 +0530

    Create 4. SumRoottoLeafNumbers.py

---
+++ Day - 6 (Binary Search Trees)/4. SumRoottoLeafNumbers.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        ans = 0
+        def dfs(node, val):
+            nonlocal ans
+            if not node:
+                return
+            val = val*10+node.val
+            if not node.left and not node.right:
+                ans+=val
+                return
+            dfs(node.left,val)
+            dfs(node.right,val)
+        if not root:
+            return 0
+        dfs(root,0)
+        return ans 
